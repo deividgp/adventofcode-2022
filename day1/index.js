c52e95c5957d2a714782e956990fdebd2530cdc6
Deividgp <dayd4@outlook.com>
2022-12-02 14:50:08 +0100

    first commit

---
+++ day1/index.js
@@ -0,0 +1,29 @@
+import fs from "node:fs";
+
+const data = fs.readFileSync("./day1/input.txt", "utf-8");
+const splitData = data.split("\n");
+let calories = 0;
+const totalCalories = [];
+
+splitData.forEach((element) => {
+  if (element != "") {
+    calories += parseInt(element);
+    return;
+  }
+  totalCalories.push(calories);
+  calories = 0;
+});
+
+const sorted = totalCalories.sort((a, b) => {
+  return b - a;
+});
+
+console.log("MAX CALORIES: " + sorted[0]);
+
+const threeMax = sorted.slice(0, 3);
+
+console.log(
+  threeMax.reduce((accumulator, value) => {
+    return accumulator + value;
+  }, 0)
+);
---
+++ day2/index.js
@@ -0,0 +1,59 @@
+import fs from "node:fs";
+
+const data = fs.readFileSync("./day2/input.txt", "utf-8");
+const splitData = data.split("\n");
+const letters = ["A", "B", "C"];
+const win = ["C", "A", "B"];
+const loss = ["B", "C", "A"];
+let totalScorePart1 = 0;
+let totalScorePart2 = 0;
+
+const scorePart1 = (opponent, own) => {
+  switch (own) {
+    // Rock
+    case "X":
+      own = "A";
+      break;
+    // Paper
+    case "Y":
+      own = "B";
+      break;
+    // Scissors
+    case "Z":
+      own = "C";
+      break;
+  }
+
+  const index = letters.indexOf(own);
+  let score = index + 1;
+
+  // Draw
+  if (opponent == own) return (score += 3);
+  // Win
+  if (opponent == win[index]) return (score += 6);
+  // Loss
+  return score;
+};
+
+const scorePart2 = (opponent, own) => {
+  switch (own) {
+    // Loss
+    case "X":
+      return loss.indexOf(opponent) + 1;
+    // Draw
+    case "Y":
+      return 3 + letters.indexOf(opponent) + 1;
+    // Win
+    case "Z":
+      return 6 + win.indexOf(opponent) + 1;
+  }
+};
+
+splitData.forEach((element) => {
+  if (element == "") return;
+  totalScorePart1 += scorePart1(element[0], element[2]);
+  totalScorePart2 += scorePart2(element[0], element[2]);
+});
+
+console.log("TOTAL SCORE PART 1: " + totalScorePart1);
+console.log("TOTAL SCORE PART 2: " + totalScorePart2);
