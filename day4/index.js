e4f2608041273d9444c0e8b0c2aa05bca7d42042
deividgp <dayd4@outlook.com>
2022-12-04 20:18:56 +0100

    day4

---
+++ day4/index.js
@@ -0,0 +1,42 @@
+import fs from "node:fs";
+
+const data = fs.readFileSync("./day4/input.txt", "utf-8");
+const splitData = data.split("\n");
+let firstAux, secondAux;
+let fullyContain = 0;
+let overlap = 0;
+
+splitData.forEach((element) => {
+  if (element == "") return;
+  element.split(",").forEach((assignment, index) => {
+    const splitAssign = assignment.split("-");
+    const first = parseInt(splitAssign[0]);
+    const second = parseInt(splitAssign[1]);
+
+    if (index == 0) {
+      firstAux = first;
+      secondAux = second;
+      return;
+    }
+
+    if (
+      (firstAux >= first && secondAux <= second) ||
+      (first >= firstAux && second <= secondAux)
+    ) {
+      fullyContain++;
+      overlap++;
+      return;
+    }
+
+    if (
+      (firstAux >= first && secondAux <= first) ||
+      (firstAux >= second && secondAux <= second) ||
+      (first >= firstAux && first <= secondAux) ||
+      (second >= firstAux && second <= secondAux)
+    )
+      overlap++;
+  });
+});
+
+console.log("FULLY CONTAIN: " + fullyContain);
+console.log("OVERLAP: " + overlap);
