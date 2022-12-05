339c61de2326534cad2d81f47765c72d71f6ce31
Deividgp <dayd4@outlook.com>
2022-12-05 09:11:27 +0100

    day5

--- day5/index.js
+++ day5/index.js
@@ -1,13 +1,53 @@
-const stack = [
-    ["G", "T", "R", "W"],
-    ["G", "C", "H", "P", "M", "S", "V", "W"],
-    ["C", "L", "T", "S", "G", "M"],
-    ["J", "H", "D", "M", "W", "R", "F"],
-    ["P", "Q", "L", "H", "S", "W", "F", "J"],
-    ["P", "J", "D", "N", "F", "M", "S"],
-    ["Z", "B", "D", "F", "G", "C", "S", "J"],
-    ["R", "T", "B"],
-    ["H", "N", "W", "L", "C"]
+import fs from "node:fs";
+
+const data = fs.readFileSync("./day5/input.txt", "utf-8");
+const splitData = data.split("\n");
+const stackPart1 = [
+  ["G", "T", "R", "W"],
+  ["G", "C", "H", "P", "M", "S", "V", "W"],
+  ["C", "L", "T", "S", "G", "M"],
+  ["J", "H", "D", "M", "W", "R", "F"],
+  ["P", "Q", "L", "H", "S", "W", "F", "J"],
+  ["P", "J", "D", "N", "F", "M", "S"],
+  ["Z", "B", "D", "F", "G", "C", "S", "J"],
+  ["R", "T", "B"],
+  ["H", "N", "W", "L", "C"],
+];
+const stackPart2 = [
+  ["G", "T", "R", "W"],
+  ["G", "C", "H", "P", "M", "S", "V", "W"],
+  ["C", "L", "T", "S", "G", "M"],
+  ["J", "H", "D", "M", "W", "R", "F"],
+  ["P", "Q", "L", "H", "S", "W", "F", "J"],
+  ["P", "J", "D", "N", "F", "M", "S"],
+  ["Z", "B", "D", "F", "G", "C", "S", "J"],
+  ["R", "T", "B"],
+  ["H", "N", "W", "L", "C"],
 ];
 
-console.log(stack);
+splitData.forEach((element) => {
+  if (element == "") return;
+  const splitStack = element.split(" ");
+  const amount = parseInt(splitStack[1]);
+  const from = parseInt(splitStack[3]);
+  const to = parseInt(splitStack[5].charAt(0));
+  let popCrates = [];
+
+  for (let index = 0; index < amount; index++) {
+    let crate = stackPart1[from - 1].pop();
+    stackPart1[to - 1].push(crate);
+    crate = stackPart2[from - 1].pop();
+    popCrates.push(crate);
+  }
+  popCrates = popCrates.reverse();
+
+  popCrates.forEach((crate) => {
+    stackPart2[to - 1].push(crate);
+  });
+});
+
+console.log("PART 1");
+console.log(stackPart1);
+console.log("------------------------------------------");
+console.log("PART 2");
+console.log(stackPart2);
